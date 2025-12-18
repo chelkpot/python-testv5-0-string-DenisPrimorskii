f0c441c7db5e60b8909042ed5092bc0c4315bf04
Денис <usovd781@gmail.com>
2025-12-18 23:07:48 +0500

    Denis

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,14 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a, b, c = map(str, input().split())
+    sim1 = a
+    sim2 = b
+    sim3 = c
+    code1 = ord(a)
+    code2 = ord(b)
+    code3 = ord(c)
+    print("Код символа " + str(sim1) + " равен " + str(code1) + "\n" + "Код символа " + str(sim2) + " равен " + str(code2) + "\n" + "Код символа " + str(sim3) + " равен " + str(code3))
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
